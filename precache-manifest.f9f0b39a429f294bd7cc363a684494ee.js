66c581e947c276e2699a719673e79ddc7cdb02f6
micorix <michal.oreziak@gmail.com>
2018-10-31 22:49:06 +0100

    Updates

---
+++ precache-manifest.f9f0b39a429f294bd7cc363a684494ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff90dc6ccbc6cbabe8df",
+    "url": "/warsawlo/static/js/runtime~main.ff90dc6c.js"
+  },
+  {
+    "revision": "a012e1d47187a080b5d1",
+    "url": "/warsawlo/static/js/main.a012e1d4.chunk.js"
+  },
+  {
+    "revision": "4250b9d1d486c9b5817a",
+    "url": "/warsawlo/static/js/1.4250b9d1.chunk.js"
+  },
+  {
+    "revision": "a012e1d47187a080b5d1",
+    "url": "/warsawlo/static/css/main.a2c09380.chunk.css"
+  },
+  {
+    "revision": "db27ad5104527603bfe784af3e989375",
+    "url": "/warsawlo/index.html"
+  }
+];
