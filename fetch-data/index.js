7685fa8af6b189b9d48dae4a0a44b06a659a3df2
micorix <michal.oreziak@gmail.com>
2018-11-25 12:19:06 +0100

    Added post-deploy processing

--- fetch-data/index.js
+++ fetch-data/index.js
@@ -9,12 +9,8 @@ const format = require('./format')
 const getDetailedLocation = require('./localization')
 const extractThresholds = require('./thresholds')
 const JSONStream = require('JSONStream')
-let counter = 0;
-prompt().then(answers => {
-  if(answers.overwrite === false)
-    process.exit()
-
-  console.log('Processing...\nIt may take a while.');
+console.log(process.argv);
+if(process.argv[2] && process.argv[2] == 'auto'){
   Promise.all([
     getDataFromTable('./fetch-data/resources/thresholds_2018.pdf'),
     getSchools()
@@ -33,7 +29,37 @@ prompt().then(answers => {
                 return school
               }))
               .pipe(JSONStream.stringify())
-              .pipe(fs.createWriteStream(`src/data/${answers.filename}`))
+              .pipe(fs.createWriteStream(`src/data/data.json`))
 
   })
-})
+}else{
+
+  let counter = 0;
+  prompt().then(answers => {
+    if(answers.overwrite === false)
+      process.exit()
+
+    console.log('Processing...\nIt may take a while.');
+    Promise.all([
+      getDataFromTable('./fetch-data/resources/thresholds_2018.pdf'),
+      getSchools()
+    ])
+    .then(values => {
+      let table = values[0]
+      let { stream } = values[1]
+        stream.on('end', () => console.log(`Processed ${counter} schools`))
+          stream
+                .pipe(format())
+                .pipe(extractThresholds(table))
+                .pipe(getDetailedLocation())
+                .pipe(es.mapSync(school => {
+                  counter++;
+                  console.log(`Done: ${school.name.full}`)
+                  return school
+                }))
+                .pipe(JSONStream.stringify())
+                .pipe(fs.createWriteStream(`src/data/${answers.filename}`))
+
+    })
+  })
+}
